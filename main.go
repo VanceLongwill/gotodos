d52ab349f2dd78df781f046c81ab7e7905cd17b5
Vance Longwill <vancelongwill@gmail.com>
2019-02-25 15:46:01 +0300

    add todo routes

--- main.go
+++ main.go
@@ -11,8 +11,50 @@ func ping(c *gin.Context) {
 	})
 }
 
+func createTodo(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "create",
+	})
+}
+
+func getAllTodos(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "getAll",
+	})
+}
+
+func getTodo(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "get",
+	})
+}
+
+func updateTodo(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "update",
+	})
+}
+
+func deleteTodo(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "delete",
+	})
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/ping", ping)
-	r.Run(":5432")
+
+	v1prefix := "/api/v1"
+
+	todoRouter := r.Group(v1prefix + "/todos")
+	{
+		todoRouter.POST("/", createTodo)
+		todoRouter.GET("/", getAllTodos)
+		todoRouter.GET("/:id", getTodo)
+		todoRouter.PUT("/:id", updateTodo)
+		todoRouter.DELETE("/:id", deleteTodo)
+	}
+
+	r.Run()
 }
