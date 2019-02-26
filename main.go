efe6f3e2999e02f35ae09ef82a4ab0dcab75c004
Vance Longwill <vancelongwill@gmail.com>
2019-02-26 22:58:58 +0300

    add user routes

--- main.go
+++ main.go
@@ -23,21 +23,32 @@ func main() {
 	apiPrefix := "api"
 	version := "v1"
 	port := "8080"
+	jwtSecret := "somesecretkey"
 
 	db, dbErr := db.Init()
 	if dbErr != nil {
 		log.Fatal("Error initialising database:\t", dbErr)
 	}
 
-	todoHandler := handlers.NewTodoHandler(db)
+	todoHandler := handlers.NewTodoHandler(db, jwtSecret)
 
 	todoRouter := app.Group(path.Join(apiPrefix, version, "todos"))
+	// per group middleware! in this case we use the custom created
+	// AuthRequired() middleware just in the "authorized" group.
+	// authorized.Use(AuthRequired())
 	{
 		todoRouter.GET("/", todoHandler.GetAll)
 		todoRouter.POST("/", todoHandler.Create)
 		todoRouter.GET("/:id", todoHandler.Get)
 		todoRouter.PUT("/:id", todoHandler.Update)
 		todoRouter.DELETE("/:id", todoHandler.Delete)
+	}
+
+	userHandler := handlers.NewUserHandler(db, jwtSecret)
+	userRouter := app.Group(path.Join(apiPrefix, version, "user"))
+	{
+		userRouter.POST("/login", userHandler.Login)
+		userRouter.POST("/register", userHandler.Register)
 	}
 
 	app.Run(fmt.Sprintf(":%s", port))
