796b06735b4e2f245332d8f3cf535753a1d92f63
Vance Longwill <vancelongwill@gmail.com>
2019-02-28 13:28:04 +0300

    add sql script to create tables

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -1,3 +1,31 @@
-CREATE DATABASE gotodos;
-CREATE USER gotodos WITH PASSWORD 'gotodos';
-GRANT ALL PRIVILEGES ON DATABASE "gotodos" to gotodos;
+-- CREATE DATABASE gotodos;
+-- CREATE USER gotodos WITH PASSWORD 'gotodos';
+-- GRANT ALL PRIVILEGES ON DATABASE "gotodos" to gotodos;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email TEXT UNIQUE NOT NULL,
+  password_hash TEXT NOT NULL
+);
+
+CREATE TABLE todos (
+  id SERIAL PRIMARY KEY,
+  title TEXT,
+  note TEXT,
+  created_at TIMESTAMP NOT NULL,
+  modified_at TIMESTAMP NOT NULL,
+  author_id INTEGER REFERENCES users(id)
+);
+
+-- @TODO: Implement a todo tagging system
+-- 
+-- CREATE TABLE tags(
+--   id SERIAL PRIMARY KEY,
+--   name VARCHAR(50) NOT NULL
+-- );
+-- 
+-- CREATE TABLE todos_tags(
+--   tag_id INTEGER REFERENCES tags(id),
+--   todo_id INTEGER REFERENCES todos(id)
+-- );
